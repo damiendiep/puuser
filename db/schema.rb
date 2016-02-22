be6f74129352533c499ae374893bda2092486511
DamienD <damien.diep@gmail.com>
2016-02-22 15:03:41 +0100

    secret key

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215095824) do
+ActiveRecord::Schema.define(version: 20160215135419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name"
+    t.string   "salle"
+    t.string   "tache"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "telephone"
+    t.integer  "group_id"
+    t.boolean  "sensei"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_attachments", force: :cascade do |t|
     t.integer  "post_id"
