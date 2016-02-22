0d0d579875e4b4aabef685ef0d7dc3352a9fc93b
DamienD <damien.diep@gmail.com>
2016-02-22 11:49:50 +0100

    keys

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{deploy@vps233848.ovh.net}
+role :web, %w{deploy@vps233848.ovh.net}
+role :db,  %w{deploy@vps233848.ovh.net}
 
 # Extended Server Syntax
 # ======================
@@ -14,7 +14,9 @@ role :db,  %w{deploy@example.com}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+set :stage, :production
+server 'vps233848.ovh.net', user: 'deploy', roles: %w{web app}
+
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -206,13 +206,12 @@ Devise.setup do |config|
   #
   # Require the `devise-encryptable` gem when using anything other than bcrypt
   # config.encryptor = :sha512
-
   # ==> Scopes configuration
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
   # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
   # config.scoped_views = false
-
+config.secret_key = '13571b21ac15c5ba5e5c1f0a9a7e25feaedbcc325478431780cd272213845c7c82a51379d3e49119b5e494eb8a7914305242615cc045b67722d6608c8a396f64' if Rails.env == 'production'
   # Configure the default scope given to Warden. By default it's the first
   # devise role declared in your routes (usually :user).
   # config.default_scope = :user
